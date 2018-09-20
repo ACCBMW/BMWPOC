53ffa9accd3d08f611d23fba72014234242255c5
ACCBMW <43430558+ACCBMW@users.noreply.github.com>
2018-09-20 11:22:00 +0530

    Your branch is up to date with 'origin/master'. 	deleted:    ossec_win.ps1 	deleted:    testfile1.txt

--- ossec_win.ps1
+++
@@ -1,6 +0,0 @@
-﻿$agent_key = "asdasd"
-$path = 'C:\Users\ossec-agent-win32-2.9.3-2912.exe'
-$config = "C:\Program Files (x86)\ossec-agent\ossec.conf"
-Start-Process -Wait -FilePath  -ArgumentList '/S' -PassThru
-Add-Content $config "`n<ossec_config>   <client>      <server-ip>0.0.0.0</server-ip>   </client> </ossec_config>"
-echo "y" | & "C:\Program Files (x86)\ossec-agent\manage_agents.exe" "-i $($agent_key)" "y`r`n"
